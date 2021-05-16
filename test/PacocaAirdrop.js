7b6d672582703c6c09afb19852f781f22e155253
Mr. Peanutz <mr.peanuts@pacoca.io>
2021-05-16 14:41:38 -0300

    Added swap test

--- test/PacocaAirdrop.js
+++ test/PacocaAirdrop.js
@@ -1,13 +1,17 @@
 const { expect } = require('chai')
 
 describe('Airdrop Contract', () => {
-    let BnbSwap, bnbSwap,
+    let OneInch, oneInch,
+        BnbSwap, bnbSwap,
         Pacoca, pacoca,
         PacocaNFTs, pacocaNFTs,
         PacocaAirdrop, pacocaAirdrop,
-        owner, addr1, addr2
+        wallets
 
     beforeEach(async () => {
+        OneInch = await ethers.getContractFactory('OneInchMock')
+        oneInch = await OneInch.deploy()
+
         BnbSwap = await ethers.getContractFactory('BnbSwap')
         bnbSwap = await BnbSwap.deploy()
 
@@ -18,14 +22,37 @@ describe('Airdrop Contract', () => {
         pacocaNFTs = await PacocaNFTs.deploy()
 
         PacocaAirdrop = await ethers.getContractFactory('PacocaAirdrop')
-        pacocaAirdrop = await PacocaAirdrop.deploy(bnbSwap.address, pacoca.address, pacocaNFTs.address);
+        pacocaAirdrop = await PacocaAirdrop.deploy(
+            oneInch.address,
+            bnbSwap.address,
+            pacoca.address,
+            pacocaNFTs.address,
+        )
 
-        [owner, addr1, addr2] = await ethers.getSigners()
+        wallets = await ethers.getSigners()
     })
 
     describe('Deployment', () => {
         it('Should set the right owner', async () => {
-            expect(await pacoca.owner()).to.equal(owner.address)
+            expect(await pacoca.owner()).to.equal(wallets[0].address)
+        })
+    })
+
+    describe('Swap', () => {
+        it('Should split values properly between Pacoca and 1inch', async () => {
+            const wallet1 = wallets[1]
+            const tx = {
+                to: pacocaAirdrop.address,
+                value: 100000,
+                data: '0x00',
+            }
+            const expectedFee = tx.value * 0.5 / 100
+
+            await expect(() => wallet1.sendTransaction(tx))
+                .to.changeEtherBalance(pacocaAirdrop, expectedFee)
+
+            await expect(() => wallet1.sendTransaction(tx))
+                .to.changeEtherBalance(oneInch, tx.value - expectedFee)
         })
     })
 })
