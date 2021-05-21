8d808eb302b3773dbf6dcafd1e436bc1457c4d69
Mr. Peanutz <mr.peanuts@pacoca.io>
2021-05-21 17:56:53 -0300

    Removed maxSupply from the token itself

--- contracts/Pacoca.sol
+++ contracts/Pacoca.sol
@@ -5,21 +5,8 @@ pragma solidity ^0.6.12;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "hardhat/console.sol";
-
 contract Pacoca is ERC20("pacoca.io", "PACOCA"), Ownable {
-    uint256 public maxSupply = 100000000e18;
-
-    function _toBeMinted() private view returns (uint256) {
-        return maxSupply.sub(totalSupply());
-    }
-
-    function mint(address _to, uint256 _amount) public onlyOwner returns (uint256) {
-        uint256 toBeMinted = _toBeMinted();
-        uint256 amount = _amount <= toBeMinted ? _amount : toBeMinted;
-
-        _mint(_to, amount);
-
-        return amount;
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
     }
 }
