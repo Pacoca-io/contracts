e1cb298fe01562593f3924e016a98716dfe346b5
Mr. Peanutz <mr.peanuts@pacoca.io>
2021-05-29 14:15:57 -0300

    Updated pacoca token name

--- contracts/Pacoca.sol
+++ contracts/Pacoca.sol
@@ -5,7 +5,7 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Pacoca is ERC20("pacoca.io", "PACOCA"), Ownable {
+contract Pacoca is ERC20("Pacoca", "PACOCA"), Ownable {
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
     }
