7a1db5867386ed74ae1acf65a7f82359726f4cae
Mr. Peanutz <mr.peanuts@pacoca.io>
2021-06-20 22:38:13 -0300

    Updated NFT contract

---
+++ contracts/PacocaCollectibles.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155Burnable.sol";
+
+contract PacocaCollectibles is ERC1155("https://api.pacoca.io/nfts/"), ERC1155Burnable, Ownable {
+    uint256 public count;
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
+        count++;
+        _mint(account, id, amount, data);
+    }
+
+    function getCollectibleURI(uint256 id) external view returns (string memory) {
+        require(id <= count, "NFT doesn't exist");
+
+        if (bytes(this.uri(id)).length == 0) {
+            return "";
+        }
+        else {
+            // abi.encodePacked is being used to concatenate strings
+            return string(abi.encodePacked(this.uri(id), uint2str(id), ".json"));
+        }
+    }
+
+    function uint2str(uint _i) internal pure returns (string memory) {
+        if (_i == 0) {
+            return "0";
+        }
+
+        uint j = _i;
+        uint len;
+
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+
+        bytes memory bStr = new bytes(len);
+        uint k = len;
+
+        while (_i != 0) {
+            k = k - 1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bStr[k] = b1;
+            _i /= 10;
+        }
+
+        return string(bStr);
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -21,11 +21,15 @@ module.exports = {
     networks: {
         bscTest: {
             url: 'https://bsc-dataseed.binance.org',
-            accounts: [process.env.BSC_TEST_PRIVATE_KEY],
+            accounts: process.env.BSC_TEST_PRIVATE_KEY
+                ? [process.env.BSC_TEST_PRIVATE_KEY]
+                : [],
         },
         bsc: {
             url: 'https://bsc-dataseed.binance.org',
-            accounts: [process.env.BSC_PRIVATE_KEY],
+            accounts: process.env.BSC_PRIVATE_KEY
+                ? [process.env.BSC_PRIVATE_KEY]
+                : [],
         },
     },
     abiExporter: {
