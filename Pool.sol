dec7612c39cfe6f88dc1c5733a73a6c850480be9
Mr. Peanutz <mr.peanuts@pacoca.io>
2021-09-28 13:07:35 -0300

    Added pool contract

---
+++ Pool.sol
@@ -0,0 +1,256 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+/**
+                                                         __
+     _____      __      ___    ___     ___     __       /\_\    ___
+    /\ '__`\  /'__`\   /'___\ / __`\  /'___\ /'__`\     \/\ \  / __`\
+    \ \ \_\ \/\ \_\.\_/\ \__//\ \_\ \/\ \__//\ \_\.\_  __\ \ \/\ \_\ \
+     \ \ ,__/\ \__/.\_\ \____\ \____/\ \____\ \__/.\_\/\_\\ \_\ \____/
+      \ \ \/  \/__/\/_/\/____/\/___/  \/____/\/__/\/_/\/_/ \/_/\/___/
+       \ \_\
+        \/_/
+
+    The sweetest DeFi portfolio manager.
+
+**/
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+contract Pool is Ownable, ReentrancyGuard {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    // Accrued token per share
+    uint256 public accTokenPerShare;
+
+    // The block number when mining starts.
+    uint256 public startBlock;
+
+    // The block number of the last pool update
+    uint256 public lastRewardBlock;
+
+    // Tokens earned per block.
+    uint256 public rewardPerBlock;
+
+    // The precision factor
+    uint256 public immutable PRECISION_FACTOR;
+
+    // The reward token
+    IERC20 public immutable rewardToken;
+
+    // The staked token
+    IERC20 public immutable stakedToken;
+
+    // Info of each user that stakes tokens (stakedToken)
+    mapping(address => UserInfo) public userInfo;
+
+    struct UserInfo {
+        uint256 amount; // How many staked tokens the user has provided
+        uint256 rewardDebt; // Reward debt
+    }
+
+    event AdminTokenRecovery(address tokenRecovered, uint256 amount);
+    event Deposit(address indexed user, uint256 amount);
+    event EmergencyWithdraw(address indexed user, uint256 amount);
+    event NewRewardPerBlock(uint256 rewardPerBlock);
+    event Withdraw(address indexed user, uint256 amount);
+
+    /*
+     * @notice Initialize the contract
+     * @param _stakedToken: staked token address
+     * @param _rewardToken: reward token address
+     * @param _rewardPerBlock: reward per block (in rewardToken)
+     * @param _startBlock: start block
+     * @param _admin: admin address with ownership
+     */
+    constructor(
+        address _stakedToken,
+        address _rewardToken,
+        uint256 _rewardTokenDecimals,
+        uint256 _rewardPerBlock,
+        uint256 _startBlock,
+        address _admin
+    ) public {
+        stakedToken = IERC20(_stakedToken);
+        rewardToken = IERC20(_rewardToken);
+        rewardPerBlock = _rewardPerBlock;
+        startBlock = _startBlock;
+
+        require(_rewardTokenDecimals < 30, "Must be inferior to 30");
+
+        PRECISION_FACTOR = uint256(10 ** (uint256(30).sub(_rewardTokenDecimals)));
+
+        // Set the lastRewardBlock as the startBlock
+        lastRewardBlock = startBlock;
+
+        // Transfer ownership to the admin address who becomes owner of the contract
+        transferOwnership(_admin);
+    }
+
+    /*
+     * @notice Deposit staked tokens and collect reward tokens (if any)
+     * @param _amount: amount to withdraw (in rewardToken)
+     */
+    function deposit(uint256 _amount) external nonReentrant {
+        UserInfo storage user = userInfo[msg.sender];
+
+        _updatePool();
+
+        if (user.amount > 0) {
+            uint256 pending = user.amount.mul(accTokenPerShare).div(PRECISION_FACTOR).sub(user.rewardDebt);
+            if (pending > 0) {
+                rewardToken.safeTransfer(address(msg.sender), pending);
+            }
+        }
+
+        if (_amount > 0) {
+            user.amount = user.amount.add(_amount);
+            stakedToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+        }
+
+        user.rewardDebt = user.amount.mul(accTokenPerShare).div(PRECISION_FACTOR);
+
+        emit Deposit(msg.sender, _amount);
+    }
+
+    /*
+     * @notice Withdraw staked tokens and collect reward tokens
+     * @param _amount: amount to withdraw (in rewardToken)
+     */
+    function withdraw(uint256 _amount) external nonReentrant {
+        UserInfo storage user = userInfo[msg.sender];
+        require(user.amount >= _amount, "Amount to withdraw too high");
+
+        _updatePool();
+
+        uint256 pending = user.amount.mul(accTokenPerShare).div(PRECISION_FACTOR).sub(user.rewardDebt);
+
+        if (_amount > 0) {
+            user.amount = user.amount.sub(_amount);
+            stakedToken.safeTransfer(address(msg.sender), _amount);
+        }
+
+        if (pending > 0) {
+            rewardToken.safeTransfer(address(msg.sender), pending);
+        }
+
+        user.rewardDebt = user.amount.mul(accTokenPerShare).div(PRECISION_FACTOR);
+
+        emit Withdraw(msg.sender, _amount);
+    }
+
+    /*
+     * @notice Withdraw staked tokens without caring about rewards rewards
+     * @dev Needs to be for emergency.
+     */
+    function emergencyWithdraw() external nonReentrant {
+        UserInfo storage user = userInfo[msg.sender];
+        uint256 amountToTransfer = user.amount;
+
+        user.amount = 0;
+        user.rewardDebt = 0;
+
+        if (amountToTransfer > 0) {
+            stakedToken.safeTransfer(address(msg.sender), amountToTransfer);
+        }
+
+        emit EmergencyWithdraw(msg.sender, user.amount);
+    }
+
+    /*
+     * @notice Stop rewards
+     * @dev Only callable by owner. Needs to be for emergency.
+     */
+    function emergencyRewardWithdraw(uint256 _amount) external onlyOwner {
+        rewardToken.safeTransfer(address(msg.sender), _amount);
+    }
+
+    /**
+     * @notice It allows the admin to recover wrong tokens sent to the contract
+     * @param _tokenAddress: the address of the token to withdraw
+     * @param _tokenAmount: the number of tokens to withdraw
+     * @dev This function is only callable by admin.
+     */
+    function recoverWrongTokens(
+        address _tokenAddress,
+        uint256 _tokenAmount
+    ) external onlyOwner {
+        require(_tokenAddress != address(stakedToken), "Cannot be staked token");
+        require(_tokenAddress != address(rewardToken), "Cannot be reward token");
+
+        IERC20(_tokenAddress).safeTransfer(address(msg.sender), _tokenAmount);
+
+        emit AdminTokenRecovery(_tokenAddress, _tokenAmount);
+    }
+
+    /*
+     * @notice Update reward per block
+     * @dev Only callable by owner.
+     * @param _rewardPerBlock: the reward per block
+     */
+    function updateRewardPerBlock(uint256 _rewardPerBlock) external onlyOwner {
+        _updatePool();
+
+        rewardPerBlock = _rewardPerBlock;
+
+        emit NewRewardPerBlock(_rewardPerBlock);
+    }
+
+    /*
+     * @notice View function to see pending reward on frontend.
+     * @param _user: user address
+     * @return Pending reward for a given user
+     */
+    function pendingReward(address _user) external view returns (uint256) {
+        UserInfo storage user = userInfo[_user];
+        uint256 stakedTokenSupply = stakedToken.balanceOf(address(this));
+
+        if (block.number > lastRewardBlock && stakedTokenSupply != 0) {
+            uint256 multiplier = _getMultiplier(lastRewardBlock, block.number);
+            uint256 cakeReward = multiplier.mul(rewardPerBlock);
+            uint256 adjustedTokenPerShare = accTokenPerShare.add(cakeReward.mul(PRECISION_FACTOR).div(stakedTokenSupply));
+
+            return user.amount.mul(adjustedTokenPerShare).div(PRECISION_FACTOR).sub(user.rewardDebt);
+        } else {
+            return user.amount.mul(accTokenPerShare).div(PRECISION_FACTOR).sub(user.rewardDebt);
+        }
+    }
+
+    /*
+     * @notice Update reward variables of the given pool to be up-to-date.
+     */
+    function _updatePool() internal {
+        if (block.number <= lastRewardBlock) {
+            return;
+        }
+
+        uint256 stakedTokenSupply = stakedToken.balanceOf(address(this));
+
+        if (stakedTokenSupply == 0) {
+            lastRewardBlock = block.number;
+            return;
+        }
+
+        uint256 multiplier = _getMultiplier(lastRewardBlock, block.number);
+        uint256 cakeReward = multiplier.mul(rewardPerBlock);
+
+        accTokenPerShare = accTokenPerShare.add(cakeReward.mul(PRECISION_FACTOR).div(stakedTokenSupply));
+        lastRewardBlock = block.number;
+    }
+
+    /*
+     * @notice Return reward multiplier over the given _from to _to block.
+     * @param _from: block to start
+     * @param _to: block to finish
+     */
+    function _getMultiplier(
+        uint256 _from,
+        uint256 _to
+    ) internal pure returns (uint256) {
+        return _to.sub(_from);
+    }
+}
