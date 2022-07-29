01544031525a107d662735d268806a848237d621
AmendoBobo <amendobobo404@protonmail.com>
2022-07-29 16:01:55 -0300

    refactor: code refactor

--- PeanutDCA.sol
+++ PeanutDCA.sol
@@ -22,140 +22,166 @@ import "@openzeppelin/contracts-upgradeable-v4/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable-v4/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable-v4/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
+import "./libraries/PacocaRouterLibrary.sol";
 import "./interfaces/IPancakeRouter02.sol";
-import "./helpers/PeanutHelpers.sol";
 
 import "hardhat/console.sol";
 
 contract PeanutDCA is UUPSUpgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
+    struct PositionInfo {
+        uint pid;
+        uint amount;
+        uint swaps;
+        uint rate;
+        uint firstSwap;
+        uint finalSwap;
+        uint lastUpdatedAt; // swap number where last updated
+    }
+
     struct PoolInfo {
         address inputToken;
         address outputToken;
         address router;
         address[] path;
-        uint totalTokensInPool;
-        uint totalAmountToSwap;
-        uint lastSwap;
+        address factory;
+        uint nextSwapAmount;
+        uint performedSwaps;
     }
 
-    struct UserInfo {
-        uint stake;
-        uint totalAmountToSwap;
-        uint amountToSwap;
-        uint rewardDebt; // TODO: rewardDebt should be on the pool that the user swapped to
-        uint lastDepositTime;
-    }
+    // user => positionId => position
+    // mapping(address => mapping(uint => PositionInfo)) internal _positionInfo;
 
-    // token address => user address => user info
-    mapping(address => mapping(address => UserInfo)) public userInfo;
+    // user => position
+    mapping(address => PositionInfo[]) internal _positionInfo;
+
+    // pid => swap number => delta
+    mapping(uint => mapping(uint => uint)) internal _poolDelta;
+
     PoolInfo[] public poolInfo;
 
-    event Deposit(address indexed user, address indexed token, uint amount);
-    event Withdraw(address indexed user, address indexed token, uint amount);
-    event CreateStrategy(uint stake, uint amout, address indexed token);
-
-    function initialize(
-        address _owner
-    ) external initializer {
+    function initialize(address _owner) external initializer {
         __ReentrancyGuard_init();
         __Ownable_init();
 
         transferOwnership(_owner);
     }
 
-    function createPoolPosition(
-        uint _amount,
-        uint _amountToSwap,
-        uint _poolId
-    ) external nonReentrant {
-        require(_amount > 0, "PeanutDCA:: amount must be greater than zero");
-        require(_amountToSwap > 0, "PeanutDCA:: amount to swap must be greater than zerp");
 
-        PoolInfo storage pool = poolInfo[_poolId];
-        UserInfo storage user = userInfo[pool.inputToken][msg.sender];
-
-        _deposit(_amount, _poolId);
-
-        user.amountToSwap = _amountToSwap;
-        pool.totalAmountToSwap = pool.totalAmountToSwap + _amountToSwap;
-    }
-
-    function deposit(uint _amount, uint _poolId) external nonReentrant {
-        _deposit(_amount, _poolId);
-    }
-
-    function withdraw(uint _amount, uint _poolId) external nonReentrant {
-        _withdraw(_amount,  _poolId);
-    }
-
-    function swap(uint[] calldata _poolIds) external onlyOwner nonReentrant {
-        for (uint8 i = 0; i < _poolIds.length; i++) {
-            PoolInfo memory pool = poolInfo[i];
-
-            IPancakeRouter02(pool.router).swapExactTokensForTokens(
-                pool.totalAmountToSwap,
-                0, // TODO: consider slippage,
-                pool.path,
-                address(this),
-                block.timestamp
-            );
-        }
-    }
+    //  --------------------
+    // | EXTERNAL FUNCTIONS |
+    //  --------------------
 
     function createPool(
         address _inputToken,
         address _outputToken,
         address _router,
-        address[] memory _path
-    ) external onlyOwner nonReentrant {
-        poolInfo.push(
-            PoolInfo({
-                inputToken: _inputToken,
-                outputToken: _outputToken,
-                router: _router,
-                path: _path,
-                totalTokensInPool: 0,
-                totalAmountToSwap: 0,
-                lastSwap: 0
-            })
-        );
+        address _factory,
+        address[] calldata _path
+    ) external onlyOwner {
+        poolInfo.push(PoolInfo({
+            inputToken: _inputToken,
+            outputToken: _outputToken,
+            router: _router,
+            factory: _factory,
+            path: _path,
+            nextSwapAmount: 0,
+            performedSwaps: 0
+        }));
     }
 
-    //INTERNAL FUNCTIONS
-    function _deposit(uint _amount, uint _poolId) internal virtual {
-        require(_amount > 0, "PeanutDCA:: amount must be greater than zero");
+    function createPosition(uint _pid, uint _amount, uint _swaps) external nonReentrant {
+        require(_poolExist(_pid), "PeanutDCA:: invalid pid");
+        require(_amount > 0, "PeanutDCA:: invalid amount");
+        require(_swaps > 0, "PeanutDCA:: invalid swaps");
 
-        PoolInfo storage pool = poolInfo[_poolId];
+        PoolInfo memory pool = poolInfo[_pid];
+
+        uint rate = _calculateRate(_amount, _swaps);
+        uint finalSwap = pool.performedSwaps + _swaps;
+
+        _increasePoolSwapAmount(_pid, rate);
+        _increasePoolDelta(_pid, rate, finalSwap + 1);
+
+        _positionInfo[msg.sender].push(_buildPosition(
+            _amount,
+            _swaps,
+            rate,
+            finalSwap,
+            pool.performedSwaps,
+            _pid
+        ));
 
         IERC20Upgradeable(pool.inputToken).safeTransferFrom(msg.sender, address(this), _amount);
-
-        UserInfo storage user = userInfo[pool.inputToken][msg.sender];
-
-        user.stake = user.stake + _amount; // TODO: current - initial balance
-        user.lastDepositTime = block.timestamp;
-
-        pool.totalTokensInPool = pool.totalTokensInPool + _amount;
-
-        emit Deposit(msg.sender, pool.inputToken, _amount);
     }
 
-    function _withdraw(uint _amount, uint _poolId) internal {
-        PoolInfo storage pool = poolInfo[_poolId];
-        UserInfo storage userInfo = userInfo[pool.inputToken][msg.sender];
+    function getPosition(address _user, uint _positionId) external view returns (PositionInfo memory) {
+        return _positionInfo[_user][_positionId];
+    }
 
-        require(_amount > 0, "PeanutDCA:: withdraw amount must be greater than zero");
-        require(_amount <= userInfo.stake, "PeanutDCA:: withdraw amount must less than or equal to token balance");
-        require(_amount <= pool.totalTokensInPool, "PeanutDCA:: not enough tokens in the contract");
+    function balanceOf(address _token) external view returns (uint balance) {
+        return IERC20Upgradeable(_token).balanceOf(address(this));
+    }
 
-        IERC20Upgradeable(pool.inputToken).safeTransfer(msg.sender, _amount);
 
-        pool.totalTokensInPool = PeanutHelpers.currentBalance(pool.inputToken);
-        userInfo.stake = userInfo.stake - _amount;
+    //  ------------------
+    // | PUBLIC FUNCTIONS |
+    //  ------------------
 
-        emit Withdraw(msg.sender, pool.inputToken, _amount);
+    function poolLength() public view returns (uint) {
+        return poolInfo.length;
+    }
+
+
+    //  --------------------
+    // | INTERNAL FUNCTIONS |
+    //  --------------------
+
+    function _buildPosition(
+        uint _amount,
+        uint _swaps,
+        uint _rate,
+        uint _finalSwap,
+        uint _firstSwap,
+        uint _pid
+    ) internal view returns (PositionInfo memory position) {
+        position = PositionInfo({
+            amount: _amount,
+            swaps: _swaps,
+            rate: _rate,
+            pid: _pid,
+            finalSwap: _finalSwap,
+            firstSwap: _firstSwap,
+            lastUpdatedAt: 0
+        });
+    }
+
+    function _increasePoolSwapAmount(uint _pid, uint _rate) internal {
+        poolInfo[_pid].nextSwapAmount += _rate;
+    }
+
+    function _increasePoolDelta(uint _pid, uint _finalSwap, uint _rate) internal {
+        _poolDelta[_pid][_finalSwap] += _rate;
+    }
+
+    function _decreasePoolSwapAmount(uint _pid, uint _rate) internal {
+        poolInfo[_pid].nextSwapAmount -= _rate;
+    }
+
+    function _decreasePoolDelta(uint _pid, uint _swapOffset, uint _rate) internal {
+        _poolDelta[_pid][_swapOffset] -= _rate;
+    }
+
+    function _poolExist(uint _pid) internal view returns (bool) {
+        return _pid < poolLength();
+    }
+
+    function _calculateRate(uint _amount, uint _swaps) internal pure returns(uint) {
+        return _amount / _swaps;
     }
 
     function _authorizeUpgrade(address) internal override onlyOwner {}
+
 }
+
